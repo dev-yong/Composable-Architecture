552f0745b18b9329fc9324558cb095fc2295b2c5
dev-yong <rhkdrmfh@gmail.com>
2020-09-21 00:06:33 +0900

    Initial Commit

---
+++ CombosableArchitecture/CombosableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombosableArchitectureApp.swift
+//  CombosableArchitecture
+//
+//  Created by 이광용 on 2020/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CombosableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombosableArchitecture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombosableArchitecture
+//
+//  Created by 이광용 on 2020/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
