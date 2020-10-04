024089f0c44f8f1bd025224c69d31de2fc733e4f
dev-yong <rhkdrmfh@gmail.com>
2020-10-04 22:32:28 +0900

    [Composable State Management: Reducers] in-out reducer

--- CombosableArchitecture/Action/CounterAction.swift
+++ CombosableArchitecture/Action/CounterAction.swift
@@ -14,14 +14,11 @@ enum CounterAction {
 }
 
 /// `reduce` 에서 영감을 받아 `Reducer`라고 명명하였다.
-func counterReducer(state: AppState, action: CounterAction) -> AppState {
-    
-    var copy = state
-    switch action {
-    case .decrTapped:
-       copy.count -= 1
-     case .incrTapped:
-       copy.count += 1
-     }
-    return copy
+func counterReducer(value: inout AppState, action: CounterAction) {
+  switch action {
+  case .decrTapped:
+    value.count -= 1
+  case .incrTapped:
+    value.count += 1
+  }
 }
--- CombosableArchitecture/Store.swift
+++ CombosableArchitecture/Store.swift
@@ -9,18 +9,18 @@ import Foundation
 
 final class Store<Value, Action>: ObservableObject {
     
-    let reducer: (Value, Action) -> Value
+    let reducer: (inout Value, Action) -> Void
     @Published var value: Value
     
     init(
         initialValue: Value,
-        reducer: @escaping (Value, Action) -> Value
+        reducer: @escaping (inout Value, Action) -> Void
     ) {
         self.value = initialValue
         self.reducer = reducer
     }
     
     func send(_ action: Action) {
-        self.value = self.reducer(self.value, action)
+        self.reducer(&self.value, action)
     }
 }
