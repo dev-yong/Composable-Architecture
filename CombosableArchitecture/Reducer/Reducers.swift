37b4b6e862893036bf511888a957fcd1d9cd825e
dev-yong <rhkdrmfh@gmail.com>
2020-10-11 15:47:55 +0900

    [Composable State Management: State Pullbacks] Combining reducers
    두 개의 reducer를 결합하는 `combine` method를 추가하였다.
    기존의 `appReducer`를 더 작은 단위로 쪼개고 다시 `combine` method를 통하여 이전과 동일한 appReducer의 역할을 수행하도록 하였다.

---
+++ CombosableArchitecture/Reducer/Reducers.swift
@@ -0,0 +1,54 @@
+//
+//  AppReducer.swift
+//  CombosableArchitecture
+//
+//  Created by 이광용 on 2020/10/11.
+//
+
+import Foundation
+
+let appReducer = combine(
+    combine(counterReducer, primeModalReducer),
+    favoritePrimesReducer
+)
+
+func counterReducer(value: inout AppState, action: AppAction) -> Void {
+    
+    switch action {
+    case . counter(.decrTapped):
+        value.count -= 1
+    case .counter(.incrTapped):
+        value.count += 1
+    default:
+        break
+    }
+}
+
+func primeModalReducer(value: inout AppState, action: AppAction) -> Void {
+    
+    switch action {
+    
+    case .primeModal(.saveFavoritePrimeTapped):
+        value.favoritePrimes.append(value.count)
+        value.activityFeed.append(.init(timestamp: Date(), type: .addedFavoritePrime(value.count)))
+    case .primeModal(.removeFavoritePrimeTapped):
+        value.favoritePrimes.removeAll(where: { $0 == value.count })
+        value.activityFeed.append(.init(timestamp: Date(), type: .removedFavoritePrime(value.count)))
+    default:
+        break
+    }
+}
+
+func favoritePrimesReducer(value: inout AppState, action: AppAction) -> Void {
+    
+    switch action {
+    case let .favoritePrimes(.deleteFavoritePrimes(indexSet)):
+        for index in indexSet {
+            let prime = value.favoritePrimes[index]
+            value.favoritePrimes.remove(at: index)
+            value.activityFeed.append(.init(timestamp: Date(), type: .removedFavoritePrime(prime)))
+        }
+    default:
+        break
+    }
+}
