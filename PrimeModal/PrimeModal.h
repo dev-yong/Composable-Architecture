796866abced612a94f4edd9c89404ede92c2e6b8
dev-yong <rhkdrmfh@gmail.com>
2020-11-02 18:25:07 +0900

    [Modular State Management: Reducers] Modularizing our reducers
    - 여태까지 구성한 모든 reducer는 구성 요소의 로직을 설명하는 독립적인 단위이다.
    - 따라서, reducer들을 자체 모듈로 추출할 수 있어야 한다.

---
+++ PrimeModal/PrimeModal.h
@@ -0,0 +1,18 @@
+//
+//  PrimeModal.h
+//  PrimeModal
+//
+//  Created by 이광용 on 2020/11/02.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrimeModal.
+FOUNDATION_EXPORT double PrimeModalVersionNumber;
+
+//! Project version string for PrimeModal.
+FOUNDATION_EXPORT const unsigned char PrimeModalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrimeModal/PublicHeader.h>
+
+
