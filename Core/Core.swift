4cfb749ab1d871562fd0a4392fa11862f1cefdc4
dev-yong <rhkdrmfh@gmail.com>
2020-11-09 00:13:40 +0900

    [Modular State Management: View State] A familiar-looking function
    - map 이라는 naming으로 변경

--- Core/Core.swift
+++ Core/Core.swift
@@ -26,7 +26,7 @@ public final class Store<Value, Action>: ObservableObject {
         self.reducer(&self.value, action)
     }
     
-    func ___<LocalValue>(
+    func map<LocalValue>(
         _ f: @escaping (Value) -> LocalValue
     ) -> Store<LocalValue, Action> {
         return Store<LocalValue, Action>(
