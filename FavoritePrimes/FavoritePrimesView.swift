7d4d91d991dfdd338d0c1270deb9b9662c78e3b1
dev-yong <rhkdrmfh@gmail.com>
2020-12-06 21:18:57 +0900

    [SideEffects: Synchronous Effects] Adding some simple side effects
    - Network request는 복잡하고 async한 side effect이므로, 간단한 synchronous side effect를 추가한다.

--- FavoritePrimes/FavoritePrimesView.swift
+++ FavoritePrimes/FavoritePrimesView.swift
@@ -20,14 +20,42 @@ public struct FavoritePrimesView: View {
     public var body: some View {
         List {
             ForEach(self.store.value, id: \.self) { prime in
-            Text("\(prime)")
-          }
-          .onDelete { indexSet in
-            
-            self.store.send(.deleteFavoritePrimes(indexSet))
-          }
+                Text("\(prime)")
+            }
+            .onDelete { indexSet in
+                
+                self.store.send(.deleteFavoritePrimes(indexSet))
+            }
         }
-          .navigationBarTitle(Text("Favorite Primes"))
+        .navigationBarTitle(Text("Favorite Primes"))
+        .navigationBarItems(
+            trailing: HStack {
+                Button("Save") {
+                    let data = try! JSONEncoder().encode(self.store.value)
+                    let documentsPath = NSSearchPathForDirectoriesInDomains(
+                        .documentDirectory, .userDomainMask, true
+                    )[0]
+                    let documentsUrl = URL(fileURLWithPath: documentsPath)
+                    let favoritePrimesUrl = documentsUrl
+                        .appendingPathComponent("favorite-primes.json")
+                    try! data.write(to: favoritePrimesUrl)
+                }
+                Button("Load") {
+                    let documentsPath = NSSearchPathForDirectoriesInDomains(
+                        .documentDirectory, .userDomainMask, true
+                    )[0]
+                    let documentsUrl = URL(fileURLWithPath: documentsPath)
+                    let favoritePrimesUrl = documentsUrl
+                        .appendingPathComponent("favorite-primes.json")
+                    guard
+                        let data = try? Data(contentsOf: favoritePrimesUrl),
+                        let favoritePrimes = try? JSONDecoder().decode([Int].self, from: data)
+                    else { return }
+                    self.store.value = favoritePrimes
+                }
+                
+            }
+        )
     }
 }
 
