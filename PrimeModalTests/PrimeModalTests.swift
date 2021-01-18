eee8df9a50cc1bb4a68157f8540e6b81feecf626
dev-yong <rhkdrmfh@gmail.com>
2021-01-18 10:16:37 +0900

    [Testing: Reducers] Testing the prime modal
    - primeModalReducer를 위한 state를 추가한다.
    - (Int, [Int])는 Equatable하지 않기 때문에 XCTAssertEqual 구문에서 컴파일 에러가 발생한다.

--- PrimeModalTests/PrimeModalTests.swift
+++ PrimeModalTests/PrimeModalTests.swift
@@ -9,24 +9,11 @@ import XCTest
 @testable import PrimeModal
 
 class PrimeModalTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    
     func testExample() throws {
-        XCTAssertTrue(false)
+        var state = (count: 2, favoritePrimes: [3, 5])
+        primeModalReducer(state: &state, action: .saveFavoritePrimeTapped)
+        XCTAssertEqual(state, (2, [3, 5, 2]))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
