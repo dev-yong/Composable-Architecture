4e534b10413c22512e78cba57696fa21702d05d6
dev-yong <rhkdrmfh@gmail.com>
2021-01-18 22:04:07 +0900

    [Testing: Reducers] Testing the prime modal
    - test 함수의 네이밍을 변경하고 removeFavoritePrimeTapped에 대한 테스트 케이스를 추가한다.
    - primeModalReducer는 side effect가 존재하지 않기에 effect 배열은 비어있다.

--- PrimeModalTests/PrimeModalTests.swift
+++ PrimeModalTests/PrimeModalTests.swift
@@ -10,13 +10,25 @@ import XCTest
 
 class PrimeModalTests: XCTestCase {
     
-    func testExample() throws {
+    func testSaveFavoritePrimesTapped() throws {
         var state = (count: 2, favoritePrimes: [3, 5])
-        primeModalReducer(state: &state, action: .saveFavoritePrimeTapped)
+        let effects = primeModalReducer(state: &state, action: .saveFavoritePrimeTapped)
         
         let (count, favoritePrimes) = state
         XCTAssertEqual(count, 2)
         XCTAssertEqual(favoritePrimes, [3, 5, 2])
+        XCTAssert(effects.isEmpty)
+    }
+    
+    func testRemoveFavoritePrimeTapped() {
+      var state = (count: 3, favoritePrimes: [3, 5])
+
+      let effects = primeModalReducer(state: &state, action: .removeFavoritePrimeTapped)
+
+      let (count, favoritePrimes) = state
+      XCTAssertEqual(count, 3)
+      XCTAssertEqual(favoritePrimes, [5])
+      XCTAssert(effects.isEmpty)
     }
     
 }
