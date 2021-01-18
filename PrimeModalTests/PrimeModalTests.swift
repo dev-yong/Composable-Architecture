df01f35d7be344485af9d9bacacfa4edd67d5a45
dev-yong <rhkdrmfh@gmail.com>
2021-01-18 10:02:17 +0900

    Testing the prime modal
    - PrimeModalTests를 위한 scheme 변경

--- PrimeModalTests/PrimeModalTests.swift
+++ PrimeModalTests/PrimeModalTests.swift
@@ -19,8 +19,7 @@ class PrimeModalTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(false)
     }
 
     func testPerformanceExample() throws {
