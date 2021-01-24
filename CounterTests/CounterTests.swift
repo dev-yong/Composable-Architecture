678a9fc05026ad6d8ad00434cd9e08748ab3f536
dev-yong <rhkdrmfh@gmail.com>
2021-01-24 19:01:20 +0900

    [Testing: Reducers] Testing the counter
    - nthPrime 과 연관된 흐름에는 asyncrhonous side effect가 존재하고 있다.
    - 반환된 effect에 대해 아무것도 확인할 수는 없지만 어느 시점에서 API 요청이 완료되고 그 결과가 저장소로 다시 공급된다는 것을 알고 있다.
    - 현재는 nthPrimeResponse 작업으로 감속기를 다시 호출하여 수동으로 작업을수행 할 수 있다.

--- CounterTests/CounterTests.swift
+++ CounterTests/CounterTests.swift
@@ -30,4 +30,74 @@ class CounterTests: XCTestCase {
         )
         XCTAssertTrue(effects.isEmpty)
     }
+    
+    func testDecrButtonTapped() {
+        var state = CounterViewState(
+            alertNthPrime: nil,
+            count: 2,
+            favoritePrimes: [3, 5],
+            isNthPrimeButtonDisabled: false
+        )
+        let effects = counterViewReducer(&state, .counter(.decrTapped))
+        
+        XCTAssertEqual(
+            state,
+            CounterViewState(
+                alertNthPrime: nil,
+                count: 1,
+                favoritePrimes: [3, 5],
+                isNthPrimeButtonDisabled: false
+            )
+        )
+        XCTAssert(effects.isEmpty)
+    }
+    
+    func testNthPrimeButtonFlow() {
+        var state = CounterViewState(
+            alertNthPrime: nil,
+            count: 2,
+            favoritePrimes: [3, 5],
+            isNthPrimeButtonDisabled: false
+        )
+        
+        var effects = counterViewReducer(&state, .counter(.nthPrimeButtonTapped))
+        
+        XCTAssertEqual(
+            state,
+            CounterViewState(
+                alertNthPrime: nil,
+                count: 2,
+                favoritePrimes: [3, 5],
+                isNthPrimeButtonDisabled: true
+            )
+        )
+        XCTAssertEqual(effects.count, 1)
+        
+        effects = counterViewReducer(&state, .counter(.nthPrimeResponse(3)))
+        
+        XCTAssertEqual(
+            state,
+            CounterViewState(
+                alertNthPrime: PrimeAlert(prime: 3),
+                count: 2,
+                favoritePrimes: [3, 5],
+                isNthPrimeButtonDisabled: false
+            )
+        )
+        XCTAssertTrue(effects.isEmpty)
+        
+        effects = counterViewReducer(&state, .counter(.alertDismissButtonTapped))
+        
+        XCTAssertEqual(
+            state,
+            CounterViewState(
+                alertNthPrime: nil,
+                count: 2,
+                favoritePrimes: [3, 5],
+                isNthPrimeButtonDisabled: false
+            )
+        )
+        XCTAssertTrue(effects.isEmpty)
+    }
+    
 }
