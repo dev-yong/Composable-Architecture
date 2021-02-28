958b745b3c3ed13c35a71a98e13029a90bdf6114
dev-yong <rhkdrmfh@gmail.com>
2021-02-28 21:42:56 +0900

    [Dependency: Injection mad composable]  Current problems
    - 통합 테스트를 위하여 module들을 import하고 파일을 정리한다.

--- PrimeModalTests/PrimeModalTests.swift
+++ PrimeModalTests/PrimeModalTests.swift
@@ -6,29 +6,10 @@
 //
 
 import XCTest
+@testable import Counter
+@testable import FavoritePrimes
 @testable import PrimeModal
 
 class PrimeModalTests: XCTestCase {
-    
-    func testSaveFavoritePrimesTapped() throws {
-        var state = (count: 2, favoritePrimes: [3, 5])
-        let effects = primeModalReducer(state: &state, action: .saveFavoritePrimeTapped)
-        
-        let (count, favoritePrimes) = state
-        XCTAssertEqual(count, 2)
-        XCTAssertEqual(favoritePrimes, [3, 5, 2])
-        XCTAssert(effects.isEmpty)
-    }
-    
-    func testRemoveFavoritePrimeTapped() {
-      var state = (count: 3, favoritePrimes: [3, 5])
 
-      let effects = primeModalReducer(state: &state, action: .removeFavoritePrimeTapped)
-
-      let (count, favoritePrimes) = state
-      XCTAssertEqual(count, 3)
-      XCTAssertEqual(favoritePrimes, [5])
-      XCTAssert(effects.isEmpty)
-    }
-    
 }
